42aec93b48f7694994b4080c23bc09c73c6621dd
matthes <leuffen@continue.de>
2017-02-09 16:54:49 +0100

    Format first harddrive

--- run_local.sh
+++ run_local.sh
@@ -4,6 +4,6 @@
 docker rm -t coreos-pxe-swarm
 docker build -t coreos-pxe-swarm .
 echo "Starting image in interactive mode...";
-docker run --net=host -e INTERFACE_BOOTSTRAP=eth2 -it coreos-pxe-swarm
+docker run --net=host -e INTERFACE_BOOTSTRAP=br0 -it coreos-pxe-swarm
 
 echo "Image closed...";
