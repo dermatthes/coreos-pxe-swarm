b160bdd4ffbae4b2082191b62662caae07232706
matthes <leuffen@continue.de>
2017-03-09 10:56:06 +0100

    Back to the basics

--- app/promote_masters.sh
+++ app/promote_masters.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set +e
+
+
 while 1; do
     sleep 30
-    docker node promote `docker node ls | tail -n +2 | awk '{print $1} '`
+    HOSTS_WITHOUT_MASTER_STATUS=`docker node ls | tail -n +2 | grep "Ready" | grep "Active" | grep -v "Leader" | grep -v "Reachable"`
+
+    if [ $($HOSTS_WITHOUT_MASTER_STATUS | wc -l) > 0 ]; then
+        docker node promote `$HOSTS_WITHOUT_MASTER_STATUS | awk '{print $1} '`
+    fi
 done
