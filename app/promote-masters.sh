de98b2557f3415d6767d56734f771e9f284a0a0a
matthes <leuffen@continue.de>
2017-03-20 13:11:08 +0100

    Promote Masters geändert

--- app/promote-masters.sh
+++ app/promote-masters.sh
@@ -8,9 +8,11 @@ set +e
 
 while [ true ]; do
     sleep 30
-    HOSTS_WITHOUT_MASTER_STATUS=`docker node ls | tail -n +2 | grep "Ready" | grep "Active" | grep -v "Leader" | grep -v "Reachable"`
+    HOSTS_WITHOUT_MASTER_STATUS=`docker node ls | tail -n +2 | grep "Ready" | grep "Active" | grep -v "Leader" | grep -v "Reachable" | grep -v "Unreachable"`
+    HOSTS_WITHOUT_MASTER_STATUS_COUNT=`docker node ls | tail -n +2 | grep "Ready" | grep "Active" | grep -v "Leader" | grep -v "Reachable" | grep -v "Unreachable" | wc -l`
 
-    if [ $($HOSTS_WITHOUT_MASTER_STATUS | wc -l) > 0 ]; then
-        docker node promote `$HOSTS_WITHOUT_MASTER_STATUS | awk '{print $1} '`
+    if [ $HOSTS_WITHOUT_MASTER_STATUS_COUNT > 0 ]; then
+        echo "Promoting new master... $HOSTS_WITHOUT_MASTER_STATUS"
+        docker node promote `echo $HOSTS_WITHOUT_MASTER_STATUS | awk '{print $1} '`
     fi
 done
